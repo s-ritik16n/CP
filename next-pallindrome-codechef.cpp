5a7aa749861b09dc7c50adb9ee857ebd57e876ac
ritik <saxenar444@gmail.com>
2018-05-15 18:36:03 +0530

    next pallindrome - stage 1

--- next-pallindrome-codechef.cpp
+++ next-pallindrome-codechef.cpp
@@ -8,8 +8,7 @@ using namespace std;
 bool checkifpallindrome(string str) {
   string rev = str;
   reverse(rev.begin(), rev.end());
-  bool val = str.compare(rev) == 0 ? true : false;
-  return val;
+  return str.compare(rev) == 0;
 }
 
 string checkifAll9s(string str) {
@@ -29,19 +28,32 @@ int main(int argc, char const *argv[]) {
   int t;
   scanf("%d", &t);
   while (t--) {
+
     string str;
     cin >> str;
     str = checkifAll9s(str);
-    int i = str.length() % 2 == 0 ? str.length()/2 - 1: str.length()/2;
+
+    if(str.length() == 1) {
+      // cout << (char)(((int)str[0] - 48) + 48)<< endl;
+      cout << (char)(((int)str[0] - 47) + 48)<< endl;
+      continue;
+    }
+
+    int i = str.length() % 2 == 0 ? str.length()/2 - 1 : str.length()/2;
     int index = str.length() - 1;
     int length = str.length();
+
     while (index > i) {
       if (str[index] > str[length - index - 1]) {
         str[index - 1] = (char)((int)str[index-1] - 47)+48; // +1
         str[index] = str[length - index - 1];
       } else if(str[index] < str[length - index - 1]) str[index] = str[length - index - 1];
       else if(str[index] == str[length - index - 1]) str[index - 1] = (char)((int)str[index-1] - 47)+48; // +1
+
       index--;
+      if (checkifpallindrome(str)) {
+        break;
+      }
     }
     cout << str << endl;
   }
