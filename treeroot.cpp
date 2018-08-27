d503e5b8eaf4e98d600442595aaa24b58ba0011f
Ritik Saxena <saxenar444@gmail.com>
2018-08-27 16:17:48 +0530

    TREEROOT final

--- treeroot.cpp
+++ treeroot.cpp
@@ -3,15 +3,15 @@
 int main() {
     int t;
     scanf("%d", &t);
-    
+
     while(t--){
         int sum = 0, a[30][2], i, n;
         scanf("%d",&n);
-        for(size_t i = 0; i < n; i++){
+        for(i = 0; i < n; i++){
             scanf("%d%d", &a[i][0], &a[i][1]);
             sum += a[i][0] - a[i][1];
         }
-        printf("%d\n", n);
+        printf("%d\n", sum);
     }
     return 0;
 }
