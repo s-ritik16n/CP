ce90748e35746cd3a5e6bb250d6e1d8d97d08743
Ritik Saxena <saxenar444@gmail.com>
2018-08-29 19:59:59 +0530

    flipcoin next commit

--- flipcoin.cpp
+++ flipcoin.cpp
@@ -6,13 +6,24 @@ int main(){
 	int i,a[n];
 	for(i = 0;i < n; i++) a[i] = -1; 
 	while(q--){
-		int a, b, c;
+		int a, b, c, sum = 0;
 		scanf("%d%d%d", &a, &b, &c);
 		if(a == 0){
-
+			for(i = b; i <= c;i++) {
+				a[i] = sum+1;
+				sum += 1;
+			}
 		}
 		else{
-		
+			if(a[b] == -1)){
+				if(a[c] == -1){
+				
+				}
+				else{
+				}	
+			} else if(a[c] == -1) {
+				
+			}
 		}
 	}
 }
