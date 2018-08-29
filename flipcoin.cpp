8fffc35496c64998c6d3062b5ba19bd6116115f4
Ritik Saxena <saxenar444@gmail.com>
2018-08-29 19:55:48 +0530

    flipcoin inital code

---
+++ flipcoin.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+	int n, q;
+	scanf("%d%d", &n,&q);
+	int i,a[n];
+	for(i = 0;i < n; i++) a[i] = -1; 
+	while(q--){
+		int a, b, c;
+		scanf("%d%d%d", &a, &b, &c);
+		if(a == 0){
+
+		}
+		else{
+		
+		}
+	}
+}
