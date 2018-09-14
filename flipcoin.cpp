b991ace6b6c0782df1f40f45576396155e17769d
Ritik Saxena <saxenar444@gmail.com>
2018-09-14 12:02:26 +0530

    new placeholder for flipcoin

--- flipcoin.cpp
+++ flipcoin.cpp
@@ -7,18 +7,22 @@
 #include<utility>
 using namespace std;
 
-int main(){
+int main() {
 	int n, q;
-	scanf("%d%d", &n,&q);
-	int a[n][2];
-	for (size_t i = 0; i < n; i++) {
-		a[i][0] = -1;
-		a[i][1] = -1;
-	}
-	std::stack<std::pair<int, int>> s;
-	while (q--) {
-		int x, y, z;
-		scanf("%d%d%d", &x, &y, &z);
-		
+	scanf("%d%d", &n, &q);
+	int arr[n], tree[4*n];
+	for(int i = 0;i < n; i++) arr[i] = 0;
+	
+	// build the tree
+
+	while(q--) {
+		int a, b, c;
+		scanf("%d%d%d", &a, &b, &c);
+		if(a == 1) {
+			// query
+		}
+		else{
+			// update
+		}
 	}
 }
